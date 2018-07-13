6538dc1ca7c48146ca7dc04199a73d2f8dfebc2a
William Jones <williamjones120862@gmail.com>
2018-07-13 10:01:41 -0400

    make data tables responsive to browser height, add root route

--- config/routes.rb
+++ config/routes.rb
@@ -28,5 +28,6 @@ Rails.application.routes.draw do
 
   get 'turkeytote/totalstx'
 
+  root 'turkeytote#totals'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
